c48bd9692838a754120afec3bfa218dd42b434fd
sbx_user1051 <sbx_user1051@169.254.51.189>
2024-06-15 17:14:17 +0000

    Sat Jun 15 17:14:17 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,7 +4,7 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        int nr1,nr2,nr3,nr4,i,suma,nrverif ;
+        int nr1,nr2,nr3,nr4,i,suma,nrverif,nr5,reducere,promo ;
         nrverif=0;
         for(i=0;i<skus.length();i++) {
             if(skus.charAt(i)=='A'){
@@ -14,6 +14,8 @@ public class CheckoutSolution {
             } if(skus.charAt(i)=='C'){
                 nrverif++;
             }if(skus.charAt(i)=='D'){
+                nrverif++;
+            }if(skus.charAt(i)=='E'){
                 nrverif++;
             }
         }
@@ -25,6 +27,7 @@ public class CheckoutSolution {
         nr2=0;
         nr3=0;
         nr4=0;
+        nr5=0;
         for(i=0;i<skus.length();i++) {
             if(skus.charAt(i)=='A'){
                 nr1++;
@@ -34,9 +37,18 @@ public class CheckoutSolution {
                 nr3++;
             }else if(skus.charAt(i)=='D'){
                 nr4++;
+            }else if(skus.charAt(i)=='E'){
+                nr5++;
             }
         }
-        suma=(nr1/3)*130+(nr1-3*(nr1/3))*50+(nr2/2)*45+(nr2-2*(nr2/2))*30+nr3*20+nr4*15;
+        reducere=nr5/2;
+        if(reducere<=nr2){
+            nr2=nr2-reducere;
+        }else{
+            nr2=0;
+        }
+        suma=(nr1/3)*130+(nr1-3*(nr1/3))*50+(nr2/2)*45+(nr2-2*(nr2/2))*30+nr3*20+nr4*15+nr5;
         return suma;
     }
 }
+
